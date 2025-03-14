b88ab11f9cad86ffadfce5d229b82211bf468414
Javier Benito <pisapipero@gmail.com>
2025-03-14 13:04:15 +0100

    Fix slider 1 image

--- src/components/Carrusel/index.tsx
+++ src/components/Carrusel/index.tsx
@@ -21,16 +21,19 @@ const ImageCarousel: React.FC<ImageCarouselProps> = ({ images, autoplay = false,
     autoplaySpeed: 3000,
     arrows: !noArrows,
   };
-
+  console.log('images', images);
   return (
     <SliderWrapper>
-      <Slider {...settings}>
-        {images.map((src, index) => (
-          <div key={index}>
-            <img src={src} alt={`Slide ${index}`} style={{ width: "90%", borderRadius: "10px", margin: "auto" }} />
-          </div>
-        ))}
-      </Slider>
+      {images.length > 1 ?
+        <Slider {...settings}>
+          {images.map((src, index) => (
+            <div key={index}>
+              <img src={src} alt={`Slide ${index}`} style={{ width: "90%", borderRadius: "10px", margin: "auto" }} />
+            </div>
+          ))}
+        </Slider> :
+        <img src={images[0]} alt={`Slide`} style={{ width: "90%", borderRadius: "10px", margin: "auto" }} />
+      }
     </SliderWrapper>
     
   );
